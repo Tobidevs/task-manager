064c5ad10b871e2dadc5209ddff834a85d5d7854
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-12 13:08:45 -0500

    base create class screen

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MdDashboard } from "react-icons/md";
 import { IoAddCircle } from "react-icons/io5";
+import { Link } from 'react-router-dom'
 
 const SideBar = () => {
   return (
@@ -8,11 +9,11 @@ const SideBar = () => {
         <div className='h-2/5 w-full pl-6 text-2xl flex flex-col gap-5 '>
             <div className='flex items-center'>
                 <MdDashboard />
-                <button className='pl-2'>Dashboard</button>
+                <Link to="/" className='pl-2'>Dashboard</Link>
             </div>
             <div className='flex items-center'>
                 <IoAddCircle/>
-                <button className='pl-2'>Create Task</button>
+                <Link to="/createtask" className='pl-2'>Create Task</Link>
             </div>
         </div>
     </div>
--- frontend/src/pages/CreateTask.jsx
+++ frontend/src/pages/CreateTask.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import TitleBar from '../components/TitleBar'
+import SideBar from '../components/SideBar'
 
 const CreateTask = () => {
   return (
-    <div>CreateTask</div>
+    <div className='bg-[#F0F4FF] h-screen w-screen'>
+      <TitleBar />
+      <div className='w-full h-full flex'>
+        <SideBar />
+        <div className='w-6/7'>
+          <div className='h-1/11 w-full text-[#2C2C2C] font-semibold text-3xl flex items-center justify-center'>Create Task</div>
+          <div className='h-10/11 flex justify-center '>
+            <form className='w-3/12 h-7/12 border border-[#D3D8DE] rounded-xl'>
+            
+            </form>
+          
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
