8b54e9f71e55fe4ffdc20214de9e0a799c4fdaa1
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-21 18:54:39 -0500

    Edit tasks modal format

--- frontend/src/components/EditTaskModal.jsx
+++ frontend/src/components/EditTaskModal.jsx
@@ -23,12 +23,12 @@ const EditTaskModal = ({ task, onClose, onUpdate, onDelete }) => {
       await axios.patch(`http://localhost:5555/tasks/${task._id}`, {
         name: name,
         description: description,
-        tags: tags
-      })
-      onUpdate()
-      onClose()
+        tags: tags,
+      });
+      onUpdate();
+      onClose();
     } catch (err) {
-      console.error("Failed to update task:", err)
+      console.error("Failed to update task:", err);
     }
   };
 
@@ -37,14 +37,18 @@ const EditTaskModal = ({ task, onClose, onUpdate, onDelete }) => {
       <div className="bg-[#F0F4FF] p-8 rounded-xl shadow-lg w-3/8 h-3/5 flex flex-col justify-center">
         <div className="flex w-full justify-center  h-1/10 relative">
           <h1 className="text-[#2C2C2C] font-semibold text-3xl ">Edit Task</h1>
-          <div className="absolute left-0 text-red-500 cursor-pointer" onClick={() => onDelete(task)}>Delete Task</div>
+          <div
+            className="absolute left-0 text-red-500 cursor-pointer"
+            onClick={() => onDelete(task)}
+          >
+            Delete Task
+          </div>
           <div
             className="absolute right-0 text-4xl cursor-pointer"
             onClick={() => onClose()}
           >
             &times;
           </div>
-          
         </div>
         <form className="h-9/10 flex flex-col gap-6" onSubmit={handleUpdate}>
           <div className="flex flex-col gap-2 h-fit w-full">
@@ -94,7 +98,12 @@ const EditTaskModal = ({ task, onClose, onUpdate, onDelete }) => {
               ))}
             </div>
           </div>
-          <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-xl cursor-pointer">Update Task</button>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-xl cursor-pointer"
+          >
+            Update Task
+          </button>
         </form>
       </div>
     </div>
