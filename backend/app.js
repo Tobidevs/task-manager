dc3f2d7f6fa7be87cdba2ff858bf3b83a0e19b3b
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-22 18:27:59 -0500

    Configured .env

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import {PORT, mongodbURL} from './config.js'
 import mongoose from 'mongoose';
 import cors from 'cors'
 import { taskRouter } from './routes/taskRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
@@ -13,11 +14,11 @@ app.use(cors())
 
 app.use('/tasks', taskRouter)
 
-app.listen(PORT)
+app.listen(process.env.PORT)
 
 
 // Mongoose connection
-mongoose.connect(mongodbURL)
+mongoose.connect(process.env.MONGODB_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'));
