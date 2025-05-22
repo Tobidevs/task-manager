aceab77e70d3632ba089256c0fc58430e1ca3f50
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-21 19:17:05 -0500

    Formatted the rest of code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import CreateTask from './pages/CreateTask'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import CreateTask from "./pages/CreateTask";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/tasks" replace />} />
-      <Route path='/tasks' element={<Dashboard />}/>
-      <Route path='/tasks/createtask' element={<CreateTask />}/>
+      <Route path="/tasks" element={<Dashboard />} />
+      <Route path="/tasks/createtask" element={<CreateTask />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
