90bd011baa03259d3adea69ed9bae7d4bb76f9b5
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-05-24 14:19:13 -0500

    Production Ready - TaskManager Deployed
    Production Ready app - Task manager deployed to Render (backend) and Vercel (frontend).

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -21,7 +21,6 @@ const TaskForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(apiUrl)
     try {
       const response = await axios.post(`${apiUrl}/tasks`, {
         name: name,
