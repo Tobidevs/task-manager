423aac3afa4286de433f9ce4e9c07b74a00a0e78
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-06-04 14:58:48 -0500

    installed daisyUI component libary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import CreateTask from "./pages/CreateTask";
+import Login from "./pages/Login"
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/tasks" replace />} />
+      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path='/login' element={<Login />} />
       <Route path="/tasks" element={<Dashboard />} />
       <Route path="/tasks/createtask" element={<CreateTask />} />
     </Routes>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="w-screen h-screen flex flex-col justify-center gap-10 items-center">
+      <p className="text-4xl font-bold ">Task Manager</p>
+      
+      {/* <div className="h-3/5 w-2/5 bg-[#F0F4FF] border border-b-1 border-[#D3D8DE] rounded-3xl flex flex-col">
+        <section className="h-2/10 border flex flex-col gap-3 justify-center items-center">
+          <h1 className="text-[#2C2C2C] text-3xl font-semibold">Welcome Back!</h1>
+          <div className="flex gap-2">
+            <p>Don't have an account yet?</p>
+            <div className="text-blue-500">Sign up now</div>
+            <div className="btn"> 
+            </div>
+          </div>
+        </section>
+        <section className="h-5/10 border">
 
-export default Login
+        </section>
+      </div> */}
+    </div>
+  );
+};
+
+export default Login;
